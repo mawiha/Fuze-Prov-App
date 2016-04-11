abb7fa8f05b9fc03418bb926721721ac991599e0
mawiha <mwhaase@gmail.com>
2016-04-11 19:15:06 -0400

    added the ability to create a new member

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,4 +4,33 @@ class MembersController < ApplicationController
     @members = Member.all
   end
 
+  def new
+    @member = Member.new
+  end
+
+  def create
+    @member = Member.new(member_params)
+
+    respond_to do |format|
+
+      if @member.save
+        format.html { redirect_to member_path(@member.id), notice: 'Member Created' }
+        format.json { render :show, status: :created, location: @movie}
+      else
+        format.html { :new_member }
+        format.json {render json: @member.errors, status: :unprocessable_enitity}
+      end
+  end
+
+  def show
+    @member = Member.find(params[:id])
+  end
 end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:first_name, :last_name, :dept, :location,:uid, :extension, :service)
+    end
+  end
+
